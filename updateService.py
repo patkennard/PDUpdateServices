96801658fb5e0038d21e0d9635ab950098e557fe
Patrick Kennard <pkennard@Patrick-Kennard-MacBook-Pro.local>
2019-10-30 10:04:07 -0700

    clean up code, added ability to pull list of services using api

--- updateService.py
+++ updateService.py
@@ -2,23 +2,18 @@
 import requests
 import csv
 import argparse
+from collections import OrderedDict
 
-### from internets, for arguments
 parser = argparse.ArgumentParser()
-
 parser.add_argument('-a', '--api', help="pagerduty api token")
 parser.add_argument('-f', '--file', help="csv filename with Services Name and Services Obfuscated ID fields")
 parser.add_argument('-c', '--change', help="change to service 'feature:setting', eg 'alert_creation:create_incidents' or 'acknowledgement_timeout:1'")
-
+parser.add_argument('-d', '--debug', dest='debug', action='store_true')
 args = parser.parse_args()
-### ^ from internets, for arguments
-# print out arguments for debugging
-#print(args.api)
-#print(args.file)
-#print(args.change)
 
+debug = False
 ### Initialize required variables
-# User can add API TOken here or use the -a commandline argument
+# User can add API Token here or use the -a commandline argument
 apiToken = ''
 # User can add services to change here, like so: services = ['PBUDURC','PR1POT7']
 # or can add a csv file as an argument to the Script
@@ -34,25 +29,17 @@ url = 'https://api.pagerduty.com/services/'
 if not args.api and not apiToken:
     print("An API Token is required. either hardcode it in apiToken variable or use -a commandline argument.")
     exit()
-if not args.file and not services:
-    print("A list of Service IDs is required. Either hardcode in services variable or use -f commandline argument.")
-    exit()
-if not args.change and not changes:
-    print("A feature to change is required. Either hardcode in changes variable or use -c commandline argument.")
-    exit()
-# if api argument then set apiToken
-if args.api:
+else:
     apiToken = args.api
-headers = { 'Content-Type': 'application/json', 'Accept': 'application/vnd.pagerduty+json;version=2', 'Authorization': 'Token token='+apiToken}
-# if change argument then set changes
 if args.change:
     changes = args.change
-    #changes = str(args.change)
+if args.debug:
+    debug = True
+
+headers = { 'Content-Type': 'application/json', 'Accept': 'application/vnd.pagerduty+json;version=2', 'Authorization': 'Token token='+apiToken}
 
 # if file argument then open file and extract Services Obfuscated ID
 if args.file:
-    ### from internets:
-    # open the file in universal line ending mode
     with open(args.file, 'r') as infile:
       # read the file as a dictionary for each row ({header : value})
       reader = csv.DictReader(infile)
@@ -63,16 +50,9 @@ if args.file:
             data[header].append(value)
           except KeyError:
             data[header] = [value]
-    # print out what was read for debugging
-    #print(data['Services Name'])
-    #print(data['Services Obfuscated ID'])
-    ### ^ from internets
-    services = data['Services Obfuscated ID']
-# print out Service IDs for debugging
-#print(services)
 
-### from internets
-# extract values from request response for verification of succedd/failure
+    services = data['Services Obfuscated ID']
+
 def extract_values(obj, key):
     """Pull all values of specified key from nested JSON."""
     arr = []
@@ -92,33 +72,24 @@ def extract_values(obj, key):
 
     results = extract(obj, arr, key)
     return results
-#  print out the request being sent for verification purposes
-def pretty_print_PUT(req):
-    """
-    At this point it is completely built and ready
-    to be fired; it is "prepared".
 
-    However pay attention at the formatting used in
-    this function because it is programmed to be pretty
-    printed and may differ from the actual request.
-    """
-    print('{}\n{}\r\n{}\r\n\r\n{}'.format(
-        '-----------START-----------',
-        req.method + ' ' + req.url,
-        '\r\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
-        req.body,
-    ))
-### ^ from internets
-
+### This is from experiments.py
+responseJson = requests.get(url, headers=headers).json()
+for s in responseJson['services']:
+    print(s['id'] + " : " + s['name'] + " -> " + s['alert_creation'] + " , " + str(s['acknowledgement_timeout'])  + " , " + str(s['auto_resolve_timeout']))
+    if s['alert_creation'] == 'create_incidents':
+        services.append(str(s['id']))
+print services
+### ^^^ This is from experiments.py
 # iterate through the service ids and send out api request to update service
 try:
     for s in services:
         print('Updating service id: '+s)
         # send API  request
-        r = requests.put(url+s, data=changes, headers=headers)
-        # print some details for troubleshooting and to confirm success
-        pretty_print_PUT(r.request)
-        print('Status Code: {code}'.format(code=r.status_code))
-        print('Changed ' + str(extract_values(r.json(), 'name')) + ' ' + changes.split(':')[0] + ' to ' + str(extract_values(r.json(), changes.split(':')[0])) + '\n\n')
+        if not debug:
+            r = requests.put(url+s, data=changes, headers=headers)
+            # print some details for troubleshooting and to confirm success
+            print('Status Code: {code}'.format(code=r.status_code))
+            print('Changed ' + str(extract_values(r.json(), 'name')) + ' ' + changes.split(':')[0] + ' to ' + str(extract_values(r.json(), changes.split(':')[0])) + '\n\n')
 except Exception as e:
     print(e)
